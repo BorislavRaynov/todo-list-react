0c74aa1fe4b36273605922c29f9725d0906f38ae
BorislavRaynov <b_raynov@abv.bg>
2024-07-07 08:42:26 +0300

    Code refactored.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,3 +1,6 @@
+import Spinner from "./Spinner";
+import ToDoItem from "./ToDoItem";
+
 export default function Main() {
     return (
         <main class="main">
@@ -9,11 +12,8 @@ export default function Main() {
                 </div>
 
                 <div className="table-wrapper">
-                    <div className="loading-container">
-                        <div className="loading-spinner">
-                            <span className="loading-spinner-text">Loading</span>
-                        </div>
-                    </div>
+                    {/* Spinner place */}
+                    {/* <Spinner /> */}
 
                     <table className="table">
                         <thead>
@@ -25,77 +25,9 @@ export default function Main() {
                         </thead>
 
                         <tbody>
-                            <tr className="todo is-completed">
-                                <td>Give dog a bath</td>
-                                <td>Complete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
 
-                            <tr className="todo is-completed">
-                                <td>Do laundry</td>
-                                <td>Complete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
-                            
-                            <tr className="todo">
-                                <td>Vacuum floor</td>
-                                <td>Incomplete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
+                            <ToDoItem />
 
-                            <tr className="todo is-completed">
-                                <td>Feed cat</td>
-                                <td>Complete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
-                            
-                            <tr className="todo">
-                                <td>Change light bulbs</td>
-                                <td>Incomplete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
-                            
-                            <tr className="todo is-completed">
-                                <td>Feed cat</td>
-                                <td>Complete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
-                            
-                            <tr className="todo">
-                                <td>Change light bulbs</td>
-                                <td>Incomplete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
-                            
-                            <tr className="todo is-completed">
-                                <td>Go to Store</td>
-                                <td>Completed</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
-                            
-                            <tr className="todo">
-                                <td>Fill gas tank</td>
-                                <td>Incomplete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr>
                         </tbody>
 
                     </table>
